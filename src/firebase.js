08f7377975c1eb877452b7437dae1a3550f7484e
Vtlvrsv <sendflyze@gmail.com>
2026-04-08 01:28:19 +0300

    fix: Firebase v9 modular

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,24 @@
+// src/firebase.js
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB4cSRFhtCSVSbLrymCeGtRsvj9XvmzU2Q",
   authDomain: "miniapp-35.firebaseapp.com",
-  projectId: "miniapp-35"
+  projectId: "miniapp-35",
+  storageBucket: "miniapp-35.firebasestorage.app",
+  messagingSenderId: "993518423599",
+  appId: "1:993518423599:web:cee1a36018a006c03980da",
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+export const db = getFirestore(app);
 
-export async function addAd(ad) {
-  await addDoc(collection(db, "ads"), ad);
-}
-
-export async function getAds() {
-  const snap = await getDocs(collection(db, "ads"));
+export async function addAd(ad) 
+{ 
+  await addDoc(collection(db, "ads"), ad); 
+} 
+  export async function getAds() 
+{ const snap = await getDocs(collection(db, "ads")); 
   return snap.docs.map(d => ({ id: d.id, ...d.data() }));
 }
